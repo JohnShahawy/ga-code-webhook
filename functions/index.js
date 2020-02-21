764a3c5a304eb20665efb2d16f8dde6e3fe40d53
John Shahawy <john.shahawy@gmail.com>
2020-02-21 10:57:54 -0500

    deploy functions

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,44 @@
 const functions = require('firebase-functions');
-const getQuotes = require('./services/GetQuotes');
+// const getQuotes = require('./services/GetQuotes');
+var ua = require('universal-analytics');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-exports.helloworld = functions.https.onRequest(async (req,res)=>{
+exports.recordGAEvent = functions.https.onRequest(async (req,res)=>{
+    //event category
+    //event action
+    // event label
+    //event value
+    // gaid
+    //messenger id
+    // const visitor= ua('UA-158268933-1')
+    const gaList = ['UA-158268933-1']
 
-    const text = req.query.text;
-    res.send(text);
+    const { category, action, label, value, gaid, messengerid } = req.headers;
+    if(gaList.indexOf(gaid) > -1){
+    const visitor= ua(gaid, messengerid)
+   try{
+    label && value 
+    ?    visitor.event(category, action, label, value).send()
+    :    visitor.event(category, action).send();
+   }
+   catch(e){
+       console.log(e)
+   }
+    if(gaid, category, action){
+    res.send('Event Recorded');
+    }
+    else {
+        res.status(500).send('Error with parameters')
+    }
+}
+else{
+    res.status(401).send('Not Authorized.  Please contact info@tactiveconsulting.com for more info.')
+}
+
 
 })
-exports.getQuotes = functions.https.onRequest(async (req,res)=>{
+// exports.getQuotes = functions.https.onRequest(async (req,res)=>{
 
-    getQuotes(req,res);
+//     getQuotes(req,res);
 
-})
+// })
+
